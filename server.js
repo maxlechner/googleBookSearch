9a07fc2bd8aaeeb2c608dfeeb91fb871f583db4b
Max Lechner <mkonlech@gmail.com>
2021-03-28 15:58:07 -0700

    update mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,9 +13,12 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // to enable Mongoose connection
-require("./config/connect");
+// require("./config/connect");
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksearch");
 
 // Define API routes here
+app.use(routes); 
 
 // Send every other request to the React app
 // Define any API routes before this runs
