5a6bdf3ac9bffa81a7d2b2512393c69db06fa3ec
Max Lechner <mkonlech@gmail.com>
2021-03-28 15:31:58 -0700

    update books model to include id and require title / author

--- models/book.js
+++ models/book.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: String,
-  author: [ String ],
+  googleBookId: String,
+  title: { type: String, required: true },
+  authors: [{ type: String, required: true }],
   description: String,
   image: String,
   link: String,
+  date: { type: Date, default: Date.now }
 });
 
 const Book = mongoose.model("Book", bookSchema);
