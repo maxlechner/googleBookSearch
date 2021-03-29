784d136d0eb5c6e5f4c52f867c39ca61d03d08cb
Max Lechner <mkonlech@gmail.com>
2021-03-28 17:02:08 -0700

    fixing API routes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,20 +1,15 @@
 import axios from "axios";
 
 const API = {
-
-  // Gets the books from Google
   getBooksAPI: function (input) {
     return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + input);
   },
-  // Gets all books
   getBooks: function () {
     return axios.get("/api/books");
   },
-  // Deletes the book with the given id
   deleteBook: function (id) {
     return axios.delete("/api/books/" + id);
   },
-  // Saves a book to the database
   saveBook: function (bookData) {
     return axios.post("/api/books", bookData);
   }
